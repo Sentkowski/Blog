16182facfbea90aee1802d621b9dd882038c3755
Szymon Sentkowski <szysen123@gmail.com>
2019-04-05 00:15:47 +0200

    Added the particles and the index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,38 @@
 import React from "react"
+import Particles from 'react-particles-js';
+import particlesConfig from "../configs/particles-config.js"
+import "../styles/reset.css"
+import "../styles/index.css"
 
-export default () => <div>Hello world!</div>
+const dummyTopics = [
+    "La, le, les, l'",
+    "On, nous",
+    "Ich, du, dich",
+    "Bla, bla, blas",
+    "Lorem ipsum"
+]
+
+export default () => (
+    <section id='' className='page-container'>
+        <div className='vert-line'/>
+        <header className='name-header'>
+            <h1 className='app-name'>Grammechs</h1>
+        </header>
+        <p className='description'>French grammar mechanised.<br/>Made interactive.<br/><span className='special-text'>Made for you.</span></p>
+        <TopicList topics={dummyTopics}/>
+        <script src="particles.js"></script>
+        <Particles params={particlesConfig} className='particles-background'/>
+    </section>
+)
+
+function TopicList(props) {
+    return (
+        <ul className='topic-list'>
+            {props.topics.map((topic) =>
+                <li key={topic} className='topic-box'>
+                    <h2>{topic}</h2>
+                </li>
+            )}
+        </ul>
+    )
+}
