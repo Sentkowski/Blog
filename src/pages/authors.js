791186be4644e6b8918ecca9e05f40cdcd1b88c6
Szymon Sentkowski <szysen123@gmail.com>
2019-04-13 00:45:18 +0200

    Updated texts and details

--- src/pages/authors.js
+++ src/pages/authors.js
@@ -13,12 +13,12 @@ export default () => (
         <PostSubtitle subtitle="Trying to make things better" />
         <TextSegment textSegments={["We are both specializing in different fields. This unique combination of skills gave birth to the idea of Grammechs."]} />
         <PostHeading heading='Camille Poilane' identifier='Poilane' />
-        <TextSegment textSegments={['Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam', 'quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.']} />
+        <TextSegment textSegments={["Hi, I'm from France and I recently turned 21. I study English and French as a Foreign Language.", "I love my language and am eager to share it worldwide (ambitious, isn’t it?). While learning how to teach French to non natives, I also give English lessons to school girls in my spare time.", "I am fond of teaching and hope to create a sphere where everyone feels free to learn in their own time. That's why I'm making sure everything you learn here is correct and illustrated with interesting examples."]} />
         <PostHeading heading='Szymon Sentkowski' identifier='Sentkowski' />
         <PictureContainer>
             <CircPicture img={Sentkowski}/>
         </PictureContainer>
-        <TextSegment textSegments={["I'm a 21 years old student of practical linguistics and copywriting, currently preparing to start a coding career.", "If something doesn't work on this website – I'm the one to blame, as I'm responsible for the technical stuff here. I'm also designing posts and interactions.", "I love to create (especially when the effects turn out to be useful to other people!), so I decided to start this project with Camille. It's not a secret that I'm still learning though, so I would be extremely grateful for any suggestions regarding the website dropped on our mail."]} />
+        <TextSegment textSegments={["I'm a 21 years old Polish student of practical linguistics and copywriting, currently preparing to start a coding career.", "If something doesn't work on this website – I'm the one to blame, as I'm responsible for the technical stuff here. I'm also designing posts and interactions.", "I love to create (especially when the effects turn out to be useful to other people!), so I decided to start this project with Camille. It's not a secret that I'm still learning though, so I would be extremely grateful for any suggestions regarding the website dropped on our mail."]} />
     </>
 )
 
