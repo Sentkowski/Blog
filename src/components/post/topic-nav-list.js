62d50537d9b3baec438a78a31b16a126fadf2397
Szymon Sentkowski <szysen123@gmail.com>
2019-04-21 03:35:23 +0200

    Paths bug fixes

--- src/components/post/topic-nav-list.js
+++ src/components/post/topic-nav-list.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
 import styles from "./topic-nav-list.module.css"
-import topicArr from "../../configs/topic-arr.js"
+import topicArr from "../../data/topic-arr.js"
 
 export default function TopicList(props) {
   let num = 1
